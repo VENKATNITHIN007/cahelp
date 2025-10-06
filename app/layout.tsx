57ddf95cedf26420118d2b7eeb293031f2ebbc14
Venkatnithin007 <venkatnithin36@gmail.com>
2025-10-06 11:54:07 +0530

    style

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,11 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
-  themeColor: '#000000',
+  themeColor: '#3b82f6',
   width: 'device-width',
   initialScale: 1,
+  maximumScale:1,
+  userScalable:false,
 };
 export default function RootLayout({
   children,
